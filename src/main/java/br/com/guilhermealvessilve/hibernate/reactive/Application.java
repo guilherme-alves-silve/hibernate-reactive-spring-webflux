f1bd002f37e275d0d84475603b842d38b96fd4fa
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-12-15 12:44:12 -0300

    Remove ea-async

--- src/main/java/br/com/guilhermealvessilve/hibernate/reactive/Application.java
+++ src/main/java/br/com/guilhermealvessilve/hibernate/reactive/Application.java
@@ -1,8 +1,6 @@
 package br.com.guilhermealvessilve.hibernate.reactive;
 
-import br.com.guilhermealvessilve.hibernate.reactive.dao.EAAsyncPersonDAO;
 import br.com.guilhermealvessilve.hibernate.reactive.dao.PersonDAO;
-import com.ea.async.Async;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,30 +12,23 @@ import org.springframework.context.annotation.ComponentScan;
 
 import javax.transaction.Transactional;
 
-import static com.ea.async.Async.await;
-
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan("br.com.guilhermealvessilve.hibernate.reactive")
 public class Application {
 
     public static void main(String[] args) {
-        Async.init();
         SpringApplication.run(Application.class, args);
     }
 
     @Autowired
     private PersonDAO dao;
 
-    @Autowired
-    private EAAsyncPersonDAO dao2;
-
     @Bean
     @Transactional
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             dao.savePerson("Guilherme");
-            dao2.savePerson("John Doe").toCompletableFuture().join();
             System.out.println("Spring Boot 2.0 working with Hibernate 5.x! ");
         };
     }
